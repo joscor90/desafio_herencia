fe7daaf4a85d0a46c7eefb52918ec30fbe7d6f09
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-07 11:12:43 -0300

    MonthlyAppointment class was defined

--- desafio_herencia.rb
+++ desafio_herencia.rb
@@ -9,9 +9,23 @@ class Appointment
 end
 
 class MonthlyAppointment < Appointment
+    attr_reader ::location, :purpose, :hour, :min, :day
+    def initialize(location, purpose, hour, min, day)
+        super(location, purpose, hour, min)
+        @day = day
+    end
+
+    def occurs_on?(day)
+        self.day == day
+    end
+
+    def to_s
+        puts "Reunión mensual en #{location} sobre #{purpose} el día #{day} a las #{hour}:#{min}"
+    end
 end
 
 class DailyAppointment < Appointment
+    
 end
 
 class OneTimeAppointment < Appointment
