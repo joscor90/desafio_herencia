4520cc3f9eb34f30e31374e88888ce6529ad1349
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-07 11:44:07 -0300

    Added tests for occurs_on? method

--- desafio_herencia.rb
+++ desafio_herencia.rb
@@ -44,7 +44,7 @@ class OneTimeAppointment < Appointment
     end
 
     def occurs_on?(day, month, year)
-        self.day == day && self.month == month && self.year = year
+        self.day == day && self.month == month && self.year == year
     end
 
     def to_s
@@ -52,9 +52,22 @@ class OneTimeAppointment < Appointment
     end
 end
 
-#testing all classes
+#testing object creation
+# puts MonthlyAppointment.new('Desafío Latam', 'OOP', 4, 30, 7)
+# puts DailyAppointment.new('Desafio Latam', 'Bases de Datos', 7, 30)
+# puts OneTimeAppointment.new('Desafio Latam', 'Módulos y Mixins', 7, 30, 8, 10, 2020)
 
-puts MonthlyAppointment.new('Desafío Latam', 'OOP', 4, 7, 30)
-puts DailyAppointment.new('Desafio Latam', 'Bases de Datos', 7, 30)
-puts OneTimeAppointment.new('Desafio Latam', 'Módulos y Mixins', 7, 30, 8, 10, 2020)
+# #testing occurs_on? method for all classes
+# puts MonthlyAppointment.new('Desafío Latam', 'OOP', 4, 30, 4).occurs_on?(4) #=> true
+# puts MonthlyAppointment.new('Desafío Latam', 'OOP', 4, 30, 4).occurs_on?(5) #=> false
+
+# puts DailyAppointment.new('Desafio Latam', 'Bases de Datos', 7, 30).occurs_on?(7, 30) #=> true
+# puts DailyAppointment.new('Desafio Latam', 'Bases de Datos', 7, 30).occurs_on?(7, 31) #=> false
+
+# puts OneTimeAppointment.new('Desafio Latam', 'Módulos y Mixins', 7, 30, 8, 10, 2020).occurs_on?(8, 10, 2020) #=> true
+# puts OneTimeAppointment.new('Desafio Latam', 'Módulos y Mixins', 7, 30, 8, 10, 2020).occurs_on?(9, 10, 2020) #=> false
+
+
+
+
 
